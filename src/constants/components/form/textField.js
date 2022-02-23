357de02a00455f1e0448b62b4910ef03b5e037e4
Kirill Zaidulin <zaidulinkirill@gmail.com>
2022-02-23 14:47:38 +0300

    fix

--- src/constants/components/form/textField.js
+++ src/constants/components/form/textField.js
@@ -1,32 +1,36 @@
 import { Box } from '@mui/material';
 import React, { useContext, useMemo } from 'react';
 import { TextValidator as BaseTextField } from 'react-material-ui-form-validator';
+import { useLocation } from 'react-router-dom';
 import { FormContext, PageContext } from '../../../contexts';
 
 export const TextField = ({ fullWidth = true, required, ...props }) => {
   const page = useContext(PageContext);
+  const location = useLocation();
+
   const translations = page.modules.find((x) => x.moduleType === 'translations');
 
   const isPassword = props.name === 'password';
+  const isSignupPage = ['/signup', '/account'].includes(location.pathname);
 
   const validators = useMemo(() => {
     if (!required) {
       return [];
     }
 
-    if (!isPassword) {
+    if (!isPassword || !isSignupPage) {
       return ['required'];
     }
 
     return ['required', 'minStringLength:10', 'matchRegexp:[a-z]', 'matchRegexp:[A-Z]', 'matchRegexp:[^a-zA-Z]'];
-  }, [isPassword, required]);
+  }, [isPassword, required, isSignupPage]);
 
   const errorMessages = useMemo(() => {
     if (!required) {
       return [];
     }
 
-    if (!isPassword) {
+    if (!isPassword || !isSignupPage) {
       return [translations.moduleVariables['[Error] Required']];
     }
 
@@ -37,7 +41,7 @@ export const TextField = ({ fullWidth = true, required, ...props }) => {
       translations.moduleVariables['[Error] Password should contain upper and lower letters'],
       translations.moduleVariables['[Error] Password should contain special symbols'],
     ];
-  }, [isPassword, required]);
+  }, [isPassword, required, isSignupPage]);
 
   return (
     <FormContext.Consumer>
