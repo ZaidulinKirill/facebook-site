62a9e83d31974301b8c3ff545d5de916cfea1bf7
Kirill Zaidulin <zaidulinkirill@gmail.com>
2022-02-24 20:55:55 +0300

    fix

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -22,7 +22,7 @@ export default function Footer() {
       <Box sx={{ height: '7px', background: 'rgb(250, 213, 73)', width: '100%' }} />
       <Box sx={{ height: '7px', background: 'rgb(225, 110, 56)', width: '100%', mt: '4px' }} />
       <Box sx={{ height: '7px', background: 'rgb(236, 177, 189)', width: '100%', mt: '4px' }} />
-      <Box sx={{ flexGrow: 1, width: '100%', px: '5%', display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ flexGrow: 1, width: '100%', px: '5%', display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
         <Link to="/" style={{ alignItems: 'center' }}>
           <StyledImage src="/tomra_logo_color.png" alt="bg" sx={{ width: { xs: '150px', sm: '200px' } }} />
         </Link>
